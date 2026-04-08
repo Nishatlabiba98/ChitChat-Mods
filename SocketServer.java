6a5ea3107179c3923cf0cd8f4fce3b86fb810560
nishat <nishatlabiba98@gmail.com>
2026-04-07 20:06:37 -0400

    chat history and chithcatlog

--- SocketServer.java
+++ SocketServer.java
@@ -6,27 +6,38 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class SocketServer {
     ServerSocket server;
     Socket sk;
     InetAddress addr;
-    
+
     ArrayList<ServerThread> list = new ArrayList<ServerThread>();
 
+    // --- ADDED: one logger shared across the whole server ---
+    static Logger logger = Logger.getLogger("ChitChatLogger");
+
     public SocketServer() {
+        // --- ADDED: set up the log file on startup ---
+        setupLogger();
+
         try {
-        	addr = InetAddress.getByName("127.0.0.1");
-        	//addr = InetAddress.getByName("192.168.43.1");
-            
-        	server = new ServerSocket(1234,50,addr);
+            addr = InetAddress.getByName("127.0.0.1");
+            //addr = InetAddress.getByName("192.168.43.1");
+
+            server = new ServerSocket(1234, 50, addr);
             System.out.println("\n Waiting for Client connection");
             SocketClient.main(null);
             while(true) {
                 sk = server.accept();
                 System.out.println(sk.getInetAddress() + " connect");
 
-                //Thread connected clients to ArrayList
+                // --- ADDED: log the new connection ---
+                logger.info("CLIENT CONNECTED: " + sk.getInetAddress());
+
                 ServerThread st = new ServerThread(this);
                 addThread(st);
                 st.start();
@@ -36,16 +47,30 @@ public class SocketServer {
         }
     }
 
+    // --- ADDED: configures the logger to write to chitchat.log ---
+    private void setupLogger() {
+        try {
+            // append=true means it won't wipe the file on every restart
+            FileHandler fh = new FileHandler("chitchat.log", true);
+            fh.setFormatter(new SimpleFormatter());
+            logger.addHandler(fh);
+            logger.setUseParentHandlers(false); // stops duplicate output to terminal
+        } catch (IOException e) {
+            System.out.println("Logger setup failed: " + e);
+        }
+    }
+    // --- end ADDED ---
+
     public void addThread(ServerThread st) {
         list.add(st);
     }
 
-    public void removeThread(ServerThread st){
-        list.remove(st); //remove
+    public void removeThread(ServerThread st) {
+        list.remove(st);
     }
 
-    public void broadCast(String message){
-        for(ServerThread st : list){
+    public void broadCast(String message) {
+        for(ServerThread st : list) {
             st.pw.println(message);
         }
     }
@@ -73,19 +98,30 @@ class ServerThread extends Thread {
             // writing
             pw = new PrintWriter(server.sk.getOutputStream(), true);
             name = br.readLine();
+
+            // --- ADDED: log nickname ---
+            SocketServer.logger.info("NICKNAME SET: " + name);
+
             server.broadCast("**["+name+"] Entered**");
 
             String data;
-            while((data = br.readLine()) != null ){
-                if(data == "/list"){
+            while((data = br.readLine()) != null) {
+                if(data == "/list") {
                     pw.println("a");
                 }
+
+                // --- ADDED: log every message ---
+                SocketServer.logger.info("MESSAGE [" + name + "]: " + data);
+
                 server.broadCast("["+name+"] "+ data);
             }
         } catch (Exception e) {
-            //Remove the current thread from the ArrayList.
             server.removeThread(this);
             server.broadCast("**["+name+"] Left**");
+
+            // --- ADDED: log disconnect ---
+            SocketServer.logger.info("CLIENT DISCONNECTED: " + name);
+
             System.out.println(server.sk.getInetAddress()+" - ["+name+"] Exit");
             System.out.println(e + "---->");
         }
